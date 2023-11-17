3c24577951539917c213be74d5ef0d0be8049375
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 14:32:16 +0300

    Updated the width of the component

--- components/Header/RecentOrders.tsx
+++ components/Header/RecentOrders.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const RecentOrders = (props: Props) => {
   return (
-    <div className='w-[30vw] bg-orange-500'>RecentOrders</div>
+    <div className='w-[28vw] bg-orange-500'>RecentOrders</div>
   )
 }
 
