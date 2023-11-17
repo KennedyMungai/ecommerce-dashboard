d6c302741212c8590ca4a8e48c971553b1a1fb5d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 12:23:55 +0300

    Did some light refactoring

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -8,13 +8,15 @@ type Props = {}
 const SideBar = (props: Props) => {
 	return (
 		<div className='w-[6vw] h-screen bg-slate-50 dark:bg-slate-900 p-4 border-r-[1px] flex flex-col justify-between'>
-			<div className='flex flex-col items-center'>
-				<Link href={'/'}>
-					<div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
-						<RxSketchLogo size={30} />
-					</div>
-				</Link>
-                <Separator className='my-5' />
+			<div className=''>
+				<div className='flex flex-col items-center'>
+					<Link href={'/'}>
+						<div className='bg-purple-800 text-white p-3 rounded-lg inline-block'>
+							<RxSketchLogo size={30} />
+						</div>
+					</Link>
+				</div>
+				<Separator className='my-5' />
 			</div>
 			<div className=''></div>
 		</div>
