339bbfe397b4c3ee199fe36f3a0289ebd8d79eb6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 16:26:42 +0300

    Made some styling updates

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Props = {}
 
 const CustomersPage = (props: Props) => {
-	return <div className='ml-[5rem] h-screen bg-slate-800 p-5'>CustomersPage</div>
+	return <div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100'>CustomersPage</div>
 }
 
 export default CustomersPage
--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 type Props = {}
 
 const SettingsPage = (props: Props) => {
-	return <div className='ml-[5rem] p-5 dark:bg-slate-800 dark-slate-100 h-screen'>SettingsPage</div>
+	return <div className='ml-[5rem] p-5 dark:bg-slate-800 bg-slate-100 h-screen'>SettingsPage</div>
 }
 
 export default SettingsPage
