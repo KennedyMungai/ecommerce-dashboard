23fb4b8b139833e7ee613063563d3ce2e176f717
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:41:11 +0300

    Made some updates for the Header componenr

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,8 +4,9 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div className='min-h-screen bg-slate-100 dark:bg-slate-700 w-full'>
-			Header
+		<div className='min-h-screen bg-slate-100 dark:bg-slate-700 w-full flex justify-between px-4 pt-4'>
+			<h2 className='font-semibold text-2xl'>DashBoard</h2>
+            <h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
 		</div>
 	)
 }
