93e625f32cb80cec9dd2548ad780b58842eb0378
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 14:20:11 +0300

    Styled the newly added cards

--- components/SideBar/TopCards/TopCards.tsx
+++ components/SideBar/TopCards/TopCards.tsx
@@ -6,34 +6,37 @@ type Props = {}
 const TopCards = (props: Props) => {
 	return (
 		<div className='flex flex-col gap-5 px-5 p-2 w-full lg:flex-row'>
-			<Card className='flex justify-between flex-grow-2 w-full p-2'>
-				<CardContent>
+			<Card className='flex justify-between w-full p-2'>
+				<CardContent className='flex justify-between w-full'>
 					<div className='flex flex-col w-full'>
 						<p className='text-2xl font-bold'>$7,846</p>
 						<p className='text-slate-400 dark:text-slate-600'>
 							Daily Revenue
 						</p>
 					</div>
+                    <p className="bg-green-200 flex items-center justify-center rounded-lg p-3">+18%</p>
 				</CardContent>
 			</Card>
-			<Card className='flex flex-grow-2 justify-between w-full p-4'>
-				<CardContent>
+			<Card className='flex justify-between w-full p-4'>
+				<CardContent className='flex justify-between w-full'>
 					<div className='flex flex-col w-full'>
 						<p className='text-2xl font-bold'>$7,846</p>
 						<p className='text-slate-400 dark:text-slate-600'>
 							Daily Revenue
 						</p>
 					</div>
+                    <p className="bg-green-200 flex items-center justify-center rounded-lg p-2">+26%</p>
 				</CardContent>
 			</Card>
-			<Card className='flex justify-between flex-grow-1 w-full p-4'>
-				<CardContent>
+			<Card className='flex justify-between w-full p-4'>
+				<CardContent className='flex justify-between w-full'>
 					<div className='flex flex-col w-full'>
 						<p className='text-2xl font-bold'>$7,846</p>
 						<p className='text-slate-400 dark:text-slate-600'>
 							Daily Revenue
 						</p>
 					</div>
+                    <p className="bg-green-200 flex items-center justify-center rounded-lg p-2">+12%</p>
 				</CardContent>
 			</Card>
 		</div>
