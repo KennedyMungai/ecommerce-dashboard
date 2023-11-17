aa907d12c9c0da1e545568c804d035e67a12099d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 12:46:12 +0300

    Added the TopCards to the Header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import TopCards from '../SideBar/TopCards/TopCards'
 
 type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div className='w-[94vw] flex justify-between p-5'>
-			<h2 className='font-semibold text-2xl'>DashBoard</h2>
-            <h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+		<div className='flex flex-col'>
+			<div className='w-[94vw] flex justify-between p-5'>
+				<h2 className='font-semibold text-2xl'>DashBoard</h2>
+				<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+			</div>
+			<TopCards />
 		</div>
 	)
 }
