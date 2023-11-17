3b506a5a5a44dbe6c9bab7db2ca6b6a0b30fcd39
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:47:56 +0300

    Used justify-between instead of justify-center

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const SideBar = (props: Props) => {
 	return (
-		<div className='w-[5rem] fixed h-screen bg-slate-50 p-4 border-r-[1px] flex flex-col justify-center'>
+		<div className='w-[5rem] fixed h-screen bg-slate-50 p-4 border-r-[1px] flex flex-col justify-between'>
 			SideBar
 		</div>
 	)
