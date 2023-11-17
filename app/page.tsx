f0a7c2434a0301b6e69f72f176b6b2e8657265de
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:49:06 +0300

    Added some styling for the main component

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SideBar from '@/components/SideBar/SideBar'
 
 export default function Home() {
   return (
-    <main className='flex flex-row'>
+    <main className='flex flex-row ml-[5rem] w-full'>
       <Header />
     </main>
   )
