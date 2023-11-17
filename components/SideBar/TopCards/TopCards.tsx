df737f8765c995bac7516ae830b740903f280fe9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 14:15:11 +0300

    Added some content for the cards

--- components/SideBar/TopCards/TopCards.tsx
+++ components/SideBar/TopCards/TopCards.tsx
@@ -5,21 +5,36 @@ type Props = {}
 
 const TopCards = (props: Props) => {
 	return (
-		<div className='flex flex-col gap-5 px-5 p-4 w-full lg:flex-row'>
-			<Card className='flex justify-between flex-grow-2 w-full p-4'>
+		<div className='flex flex-col gap-5 px-5 p-2 w-full lg:flex-row'>
+			<Card className='flex justify-between flex-grow-2 w-full p-2'>
 				<CardContent>
-
-                </CardContent>
+					<div className='flex flex-col w-full'>
+						<p className='text-2xl font-bold'>$7,846</p>
+						<p className='text-slate-400 dark:text-slate-600'>
+							Daily Revenue
+						</p>
+					</div>
+				</CardContent>
 			</Card>
 			<Card className='flex flex-grow-2 justify-between w-full p-4'>
 				<CardContent>
-
-                </CardContent>
+					<div className='flex flex-col w-full'>
+						<p className='text-2xl font-bold'>$7,846</p>
+						<p className='text-slate-400 dark:text-slate-600'>
+							Daily Revenue
+						</p>
+					</div>
+				</CardContent>
 			</Card>
 			<Card className='flex justify-between flex-grow-1 w-full p-4'>
 				<CardContent>
-
-                </CardContent>
+					<div className='flex flex-col w-full'>
+						<p className='text-2xl font-bold'>$7,846</p>
+						<p className='text-slate-400 dark:text-slate-600'>
+							Daily Revenue
+						</p>
+					</div>
+				</CardContent>
 			</Card>
 		</div>
 	)
