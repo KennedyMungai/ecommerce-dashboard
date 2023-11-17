2711e39d942ca6e30720ec7fbc3b967591b58faa
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:36:34 +0300

    Added a width of 5vw

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div className='min-h-screen bg-slate-100 dark:bg-slate-700'>
+		<div className='min-h-screen bg-slate-100 dark:bg-slate-700 w-[95vw]'>
 			Header
 		</div>
 	)
