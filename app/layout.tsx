75a037621a582eafcfd15f9fcacd04f7e780dc24
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:27:24 +0300

    Set the ThemeProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ThemeProvider from '@/providers/ThemeProvider'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
@@ -5,18 +6,20 @@ import './globals.css'
 const open_sans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'User Dashboard',
-  description: 'A simple User Dashboard',
+	title: 'User Dashboard',
+	description: 'A simple User Dashboard'
 }
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={open_sans.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={open_sans.className}>
+				<ThemeProvider>{children}</ThemeProvider>
+			</body>
+		</html>
+	)
 }
