ab682278bd239c4f82994c007ad3f924c8a9afeb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 16:17:00 +0300

    Fixed the dashboard layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Header from '@/components/Header/Header'
-import SideBar from '@/components/SideBar/SideBar'
 
 export default function Home() {
   return (
-    <main className='flex bg-slate-100 dark:bg-slate-800 min-h-screen'>
-      <SideBar />
+    <main className='flex bg-slate-100 dark:bg-slate-800 ml-[5rem] h-screen'>
       <Header />
     </main>
   )
