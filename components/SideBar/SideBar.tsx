63543f9c916881911ba0f16bce946f08dc21cf4b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 12:12:09 +0300

    Made some changes

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,11 +1,20 @@
+import Link from 'next/link'
 import React from 'react'
+import { RxSketchLogo } from 'react-icons/rx'
 
 type Props = {}
 
 const SideBar = (props: Props) => {
 	return (
-		<div className='w-[7.5vw] h-screen bg-slate-50 dark:bg-slate-900 p-4 border-r-[1px] flex flex-col justify-between'>
-			SideBar
+		<div className='w-[6vw] h-screen bg-slate-50 dark:bg-slate-900 p-4 border-r-[1px] flex flex-col justify-between'>
+			<div className='flex flex-col items-center'>
+				<Link href={'/'}>
+					<div className=''>
+						<RxSketchLogo size={30} />
+					</div>
+				</Link>
+			</div>
+			<div className=''></div>
 		</div>
 	)
 }
