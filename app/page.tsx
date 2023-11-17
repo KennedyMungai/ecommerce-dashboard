2a9adafc365ab6d50c247bd125aeea3ebe0c3589
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:37:23 +0300

    Rendered out teh SideBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/components/Header/Header'
+import SideBar from '@/components/SideBar/SideBar'
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <SideBar />
     </main>
   )
 }
