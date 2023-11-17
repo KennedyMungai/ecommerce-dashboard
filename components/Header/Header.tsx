6542dd353eacd42e1415b84ce978a97738a00bbd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:35:32 +0300

    Styled the header component

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <div>Header</div>
-  )
+	return (
+		<div className='min-h-screen bg-slate-100 dark:bg-slate-700'>
+			Header
+		</div>
+	)
 }
 
 export default Header
