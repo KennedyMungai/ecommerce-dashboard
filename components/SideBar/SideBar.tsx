c8d6cfddf3fbff532c8a1b153a5432acde059fb5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 12:39:09 +0300

    Styled all sidenbar components to use darkmode

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -22,28 +22,28 @@ const SideBar = (props: Props) => {
 				<Separator className='my-5' />
 				<div className='flex flex-col items-center'>
 					<Link href={'/'}>
-						<div className='bg-slate-200 hover:bg-slate-200/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
+						<div className='bg-slate-200 hover:bg-slate-200/50 dark:bg-slate-700 dark:hover:bg-slate-700/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
 							<RxDashboard size={20} />
 						</div>
 					</Link>
 				</div>
 				<div className='flex flex-col items-center'>
 					<Link href={'/customers'}>
-						<div className='bg-slate-200 hover:bg-slate-200/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
+						<div className='bg-slate-200 hover:bg-slate-200/50 dark:bg-slate-700 dark:hover:bg-slate-700/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
 							<RxPerson size={20} />
 						</div>
 					</Link>
 				</div>
 				<div className='flex flex-col items-center'>
 					<Link href={'/orders'}>
-						<div className='bg-slate-200 hover:bg-slate-200/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
+						<div className='bg-slate-200 hover:bg-slate-200/50 dark:bg-slate-700 dark:hover:bg-slate-700/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
 							<HiOutlineShoppingBag size={20} />
 						</div>
 					</Link>
 				</div>
 				<div className='flex flex-col items-center'>
 					<Link href={'/settings'}>
-						<div className='bg-slate-200 hover:bg-slate-200/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
+						<div className='bg-slate-200 hover:bg-slate-200/50 dark:bg-slate-700 dark:hover:bg-slate-700/50 cursor-pointer my-2 p-2 rounded-lg inline-block'>
 							<FiSettings size={20} />
 						</div>
 					</Link>
