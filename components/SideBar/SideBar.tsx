a7132638c76ea7d22fb38957ae16fb9c55320b1f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-17 11:47:15 +0300

    Styliing the Sidebar component

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 type Props = {}
 
 const SideBar = (props: Props) => {
-  return (
-    <div className='w-[5rem]'>SideBar</div>
-  )
+	return (
+		<div className='w-[5rem] fixed h-screen bg-slate-50 p-4 border-r-[1px] flex flex-col justify-center'>
+			SideBar
+		</div>
+	)
 }
 
 export default SideBar
