87945783695373c85b0995d4c2d3e6d5cb211c3a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:24:47 +0300

    Made some changes to the text

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -8,7 +8,7 @@ const CustomersPage = (props: Props) => {
 	return (
 		<div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100'>
 			<div className='flex justify-between items-center'>
-				<h2>Customers</h2>
+				<h2 className='text-2xl uppercase'>Customers</h2>
 				<h2>Welcome Back, Motherfucker</h2>
 			</div>
 		</div>
