028c9498308866cf60a9cbf9e6d732b566c8b617
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:42:05 +0300

    Adjusted the height of the page component

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 const CustomersPage = (props: Props) => {
 	return (
-		<div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100 text-slate-700 dark:text-slate-400'>
+		<div className='ml-[5rem] min-h-screen dark:bg-slate-800 p-5 bg-slate-100 text-slate-700 dark:text-slate-400'>
 			<div className='flex justify-between items-center'>
 				<h2 className='text-2xl uppercase font-semibold'>Customers</h2>
 				<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
