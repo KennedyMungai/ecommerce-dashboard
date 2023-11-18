bf7db81e91ddbb1e10205837cef8dd3c9931c4e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:40:20 +0300

    Made some markup and styling changes to the react component

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -16,16 +16,16 @@ const CustomersPage = (props: Props) => {
 					<div className='flex justify-between'>
 						<span className='font-semibold'>Name</span>
 						<span className='font-semibold'>Email</span>
-						<span className='font-semibold hidden lg:display-flex'>Last Order</span>
-						<span className='font-semibold hidden lg:display-flex'>Method</span>
+						<span className='font-semibold hidden lg:flex'>Last Order</span>
+						<span className='font-semibold hidden lg:flex'>Method</span>
 						<span className='hidden lg:flex'></span>
 					</div>
 					<ul className='list-none'>
 						{data.map((order, id) => {
 							return (
 								<li key={id} className='bg-slate-100 hover:bg-slate-100/50 rounded-lg my-3 p-2 flex justify-between items-center cursor-pointer'>
-									<div className="">
-										<div className="">
+									<div className="flex items-center">
+										<div className="bg-slate-200 dark:bg-slate-600 p-3 rounded-lg">
 											<BsPersonFill className='text-slate-700 dark:text-slate-400' />
 										</div>
 									</div>
