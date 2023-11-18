bc02b9b8ec37932915f00fc0093d5a214e3a1b5f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:29:30 +0300

    Properly rendered the table names

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -6,10 +6,21 @@ type Props = {}
 
 const CustomersPage = (props: Props) => {
 	return (
-		<div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100'>
+		<div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100 text-slate-700 dark:text-slate-400'>
 			<div className='flex justify-between items-center'>
 				<h2 className='text-2xl uppercase font-semibold'>Customers</h2>
 				<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+			</div>
+			<div className='p-4'>
+				<div className='w-full m-auto p-4 rounded-lg bg-white overflow-y-auto'>
+					<div className='flex justify-between'>
+						<span className='font-semibold'>Name</span>
+						<span className='font-semibold'>Email</span>
+						<span className='font-semibold'>Last Order</span>
+						<span className='font-semibold'>Method</span>
+						<span></span>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
