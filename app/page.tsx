37bfc517f3040455a67b0609cb2910adff694008
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:56:21 +0300

    Made a lot of styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header/Header'
 
 export default function Home() {
   return (
-    <main className='flex bg-slate-100 dark:bg-slate-800 ml-[5rem] min-h-screen lg:ml-[5rem]'>
+    <main className='flex bg-slate-100 dark:bg-slate-800 ml-[5rem] min-h-screen lg:ml-[5rem] max-w-screen lg:max-screen-lg md:max-screen-md'>
       <Header />
     </main>
   )
