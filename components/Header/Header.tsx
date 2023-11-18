fa89cc04319d9be49a280cf9f5ff338200ac39f9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 09:01:56 +0300

    Set the height of the components to screen

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,7 +7,7 @@ type Props = {}
 
 const Header = (props: Props) => {
 	return (
-		<div className='flex flex-col'>
+		<div className='flex flex-col h-screen'>
 			<div className='w-[94vw] flex justify-between p-5'>
 				<h2 className='font-semibold text-2xl'>DashBoard</h2>
 				<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
