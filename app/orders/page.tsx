23942f968b60e6197ef921f9d0ea60531fa2cdf1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 11:06:21 +0300

    Added some styling updates for the title text

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -6,7 +6,12 @@ import { data } from '@/data/data'
 type Props = {}
 
 const OrdersPage = (props: Props) => {
-	return <div className='ml-[5rem] dark:bg-slate-800 p-5 min-h-screen max-w-screen bg-slate-100'>OrdersPage</div>
+	return <div className='ml-[5rem] dark:bg-slate-800 p-5 min-h-screen max-w-screen bg-slate-100'>
+		<div className="flex items-center justify-between text-slate-800 dark:text-slate-100">
+			<h2 className='font-semibold text-2xl'>Orders</h2>
+			<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+		</div>
+	</div>
 }
 
 export default OrdersPage
