7b66c4093c23f0d60f062aad63500638575446b4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 10:23:27 +0300

    Made some changes for the customers page

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -1,9 +1,18 @@
+import { data } from '@/data/data'
 import React from 'react'
+import { BsPersonFill, BsThreeDotsVertical } from 'react-icons/bs'
 
 type Props = {}
 
 const CustomersPage = (props: Props) => {
-	return <div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100'>CustomersPage</div>
+	return (
+		<div className='ml-[5rem] h-screen dark:bg-slate-800 p-5 bg-slate-100'>
+			<div className=''>
+				<h2>Customers</h2>
+				<h2>Welcome Back, Motherfucker</h2>
+			</div>
+		</div>
+	)
 }
 
 export default CustomersPage
