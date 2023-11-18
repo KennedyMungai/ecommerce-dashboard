624749e1f5acdbc3ff92bbd4c9cac01a03076973
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-11-18 11:12:42 +0300

    Made some updates

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import {FaShoppingBag} from 'react-icons/fa'
-import {BsThreeDotsVertical} from 'react-icons/bs'
+import { FaShoppingBag } from 'react-icons/fa'
+import { BsThreeDotsVertical } from 'react-icons/bs'
 import { data } from '@/data/data'
 
 type Props = {}
 
 const OrdersPage = (props: Props) => {
-	return <div className='ml-[5rem] dark:bg-slate-800 p-5 min-h-screen max-w-screen bg-slate-100'>
-		<div className="flex items-center justify-between text-slate-800 dark:text-slate-100">
-			<h2 className='font-semibold text-2xl'>Orders</h2>
-			<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+	return (
+		<div className='ml-[5rem] dark:bg-slate-800 p-5 min-h-screen max-w-screen bg-slate-200 text-slate-800 dark:text-slate-100'>
+			<div className='flex items-center justify-between'>
+				<h2 className='font-semibold text-2xl'>Orders</h2>
+				<h2 className='font-semibold'>Welcome Back, Motherfucker</h2>
+			</div>
+			<div className='w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto text-slate-700'>
+				<div className='my-3 p-2 items-center justify-between cursor-pointer'>
+					<span>Order</span>
+					<span>Status</span>
+					<span>Last Order</span>
+					<span>Method</span>
+					<span></span>
+				</div>
+			</div>
 		</div>
-	</div>
+	)
 }
 
 export default OrdersPage
